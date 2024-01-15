b1e5d9e5caba00d5045427196ce3c29bbe468021
classicrocker883 <andrewleduc88@yahoo.com>
2024-01-15 09:10:20 -0500

    Update - compile-configs / UBL_NoPro

--- configurations/Voxelab Aquila/UBL/Configuration.h
+++ configurations/Voxelab Aquila/UBL/Configuration.h
@@ -2433,10 +2433,10 @@
 //#define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_3_LABEL       "Warmup"
-#define PREHEAT_3_TEMP_HOTEND 200
-#define PREHEAT_3_TEMP_BED     50
-#define PREHEAT_3_FAN_SPEED     0
+//#define PREHEAT_3_LABEL       "Warmup"
+//#define PREHEAT_3_TEMP_HOTEND 200
+//#define PREHEAT_3_TEMP_BED     50
+//#define PREHEAT_3_FAN_SPEED     0
 
 //#define PREHEAT_4_LABEL       "TPU"
 //#define PREHEAT_4_TEMP_HOTEND 230
