754e3d095870bb165b3a778d6a1e2ea479af81fc
classicrocker883 <andrewleduc88@yahoo.com>
2024-04-08 13:41:48 -0400

    Update - X3 to Induct

--- configurations/Voxelab Aquila/X3/Version.h
+++ configurations/Voxelab Aquila/X3/Version.h
@@ -34,7 +34,7 @@
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION "AquilaX3-UBL"
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION "Aquila-UBL-Inductive"
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
@@ -49,7 +49,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "Aquila X3"
+#define MACHINE_NAME "Aquila"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
